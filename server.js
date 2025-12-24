a0c566bb15aef97bccc2c64b3812719ae8e95058
0Radian0 <adrianpietka0481@gmail.com>
2025-12-24 22:27:31 +0100

    poprawa cors w serverjs

--- server.js
+++ server.js
@@ -30,9 +30,12 @@ const app = express();
  */
 app.use(cors({
   origin: "https://szermierka-historyczna-lublin.netlify.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+app.options("*", cors());
 
 
 
